d73b8c2a6300274ef27554c10e7397e7e1d562c7
zachary_erskine@bmc.com <zachary_erskine@bmc.com>
2020-06-23 19:16:39 +0300

    init commit

---
+++ src/test/java/com/bmc/cm/soaptest/wsclient/WsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmc.cm.soaptest.wsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
