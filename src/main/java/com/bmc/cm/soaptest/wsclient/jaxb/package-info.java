a40f5d1498f2a9ec23577bba54512b32758f916d
zachary_erskine@bmc.com <zachary_erskine@bmc.com>
2020-06-24 11:20:02 +0300

    integrate existing wsdl for jaxb

---
+++ src/main/java/com/bmc/cm/soaptest/wsclient/jaxb/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.bmc.com/cm/soaptest", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.bmc.cm.soaptest.wsclient.jaxb;
