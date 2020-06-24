5463c954dbb0cd45911bbf66e4ff8532aad63053
zachary_erskine@bmc.com <zachary_erskine@bmc.com>
2020-06-24 09:38:34 +0300

    integrate existing wsdl for spring ws

--- src/main/java/com/bmc/cm/soaptest/wsclient/WsClientApplication.java
+++ src/main/java/com/bmc/cm/soaptest/wsclient/WsClientApplication.java
@@ -1,7 +1,11 @@
 package com.bmc.cm.soaptest.wsclient;
 
+import com.bmc.cm.soaptest.wsclient.springws.CountryClient;
+import com.bmc.cm.soaptest.wsdl.GetCountryResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WsClientApplication {
@@ -10,4 +14,16 @@ public class WsClientApplication {
 		SpringApplication.run(WsClientApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner lookup(CountryClient quoteClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = quoteClient.getCountry(country);
+			System.err.println(response.getCountry().getCurrency());
+		};
+	}
 }
