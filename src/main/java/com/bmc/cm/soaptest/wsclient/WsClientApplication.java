d67c2ea00dd8f3b0d7f80d5a99b91e8f4f016ba4
zachary_erskine@bmc.com <zachary_erskine@bmc.com>
2020-06-24 11:20:02 +0300

    integrate existing wsdl for jaxb

--- src/main/java/com/bmc/cm/soaptest/wsclient/WsClientApplication.java
+++ src/main/java/com/bmc/cm/soaptest/wsclient/WsClientApplication.java
@@ -1,11 +1,17 @@
 package com.bmc.cm.soaptest.wsclient;
 
+import com.bmc.cm.soaptest.wsclient.jaxb.CountriesPort;
+import com.bmc.cm.soaptest.wsclient.jaxb.CountriesPortService;
 import com.bmc.cm.soaptest.wsclient.springws.CountryClient;
+import com.bmc.cm.soaptest.wsdl.GetCountryRequest;
 import com.bmc.cm.soaptest.wsdl.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.net.URL;
 
 @SpringBootApplication
 public class WsClientApplication {
@@ -15,15 +21,27 @@ public class WsClientApplication {
 	}
 
 	@Bean
-	CommandLineRunner lookup(CountryClient quoteClient) {
+	CommandLineRunner lookup(
+			@Value("${client}")String client,CountryClient quoteClient) {
+
 		return args -> {
 			String country = "Spain";
 
 			if (args.length > 0) {
 				country = args[0];
 			}
-			GetCountryResponse response = quoteClient.getCountry(country);
-			System.err.println(response.getCountry().getCurrency());
+			if (client.equals("ws")) {
+				GetCountryResponse response = quoteClient.getCountry(country);
+				System.err.println(response.getCountry().getCurrency());
+			} else {
+				URL url = new URL("http://localhost:8080/ws/countries.wsdl");
+				CountriesPortService countriesPortService = new CountriesPortService(url);
+				com.bmc.cm.soaptest.wsclient.jaxb.GetCountryRequest getCountryRequest = new com.bmc.cm.soaptest.wsclient.jaxb.GetCountryRequest();
+				getCountryRequest.setName(country);
+				CountriesPort a = countriesPortService.getCountriesPortSoap11();
+				com.bmc.cm.soaptest.wsclient.jaxb.GetCountryResponse b =a.getCountry(getCountryRequest);
+				System.out.println(b.getCountry().getCurrency());
+			}
 		};
 	}
 }
